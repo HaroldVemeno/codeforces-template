3dc8390d5abd4395c0a7f2456ed779066635b9cb
Štěpán Mikéska <haroldvemeno@email.cz>
2022-01-22 15:00:33 +0100

    initial

---
+++ a.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i, n) for(int i = 0; i<(n); ++i)
+#define FOR1(i, n) for(int i = 1; i<=(n); ++i)
+#define DEB(x) cerr << #x << ": " << x << '\n';
+#define all(x) begin(x), end(x)
+using ll = int64_t;
+//#define int ll
+
+void solve() {
+    //////
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tests;
+    cin >> tests;
+    while(tests-- > 0) {
+        solve();
+    }
+
+}
